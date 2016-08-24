f3dbc6351d19b4427baf8a8b5faef79d9611724e
many258 <many258@naver.com>
2016-08-24 23:49:31 +0900

    Resource/Enemy/Player
    리소스 폴더 추가
    총탄 발사
    콜라이더 크기 증가

--- TurtleHunter/Assets/Scripts/Input2.cs
+++ TurtleHunter/Assets/Scripts/Input2.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Input2 : MonoBehaviour {
 
+    public GameObject o_bullet;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +11,16 @@ public class Input2 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.touchCount == 1)//터치
+        //if(Input.GetMouseButton(0))//임시방편 마우스 클릭
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+                Instantiate(o_bullet);
+            Debug.Log("Touchded");
+        }
+    }
+    void Touched()
+    {
+
+    }
 }
