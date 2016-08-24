223658a51d34d0834af8b2d2219f18114cd6af79
jc5201 <jch5201@naver.com>
2016-08-25 01:31:32 +0900

    Input(gyro)

---
+++ TurtleHunter/Assets/Scripts/InputScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputScript : MonoBehaviour {
+
+
+    GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Player.transform.Rotate(-1 * Input.gyro.rotationRateUnbiased.x, -1 * Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRateUnbiased.z);
+	}
+}
