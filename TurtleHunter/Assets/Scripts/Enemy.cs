7cb5f742c3b0147618272602bd67d5d21104b947
many258 <many258@naver.com>
2016-08-26 00:29:51 +0900

    UI / Arrow
    나침반 수정
    UI 중간작업

--- TurtleHunter/Assets/Scripts/Enemy.cs
+++ TurtleHunter/Assets/Scripts/Enemy.cs
@@ -7,10 +7,20 @@ public class Enemy : MonoBehaviour {
     public bool b_isExist;
     private GameObject Arrow_Position;
     public GameObject Arrow;
+    public int Enemy_HP;
     private GameObject _Player;
     private GameObject obj;
     // Use this for initialization
     void Start () {
+        switch (this.tag)
+        {
+            case "Enemy":
+                Enemy_HP = 1;
+                break;
+            case "Boss":
+                Enemy_HP = 3;
+                break;
+        }
         _Player = GameObject.Find("Player");
         Arrow_Position = GameObject.Find("ArrowPosition");
         obj = Instantiate(Arrow, Arrow_Position.transform.position, Arrow_Position.transform.rotation) as GameObject;
@@ -25,10 +35,18 @@ public class Enemy : MonoBehaviour {
             Debug.Log("invisible!");
             //나중에 이부분에 화살표도 안보이도록 설정할것!
         }
+        if(Enemy_HP <= 0)
+        {
+            Destroy(this);
+            GameObject.Find("GameRoot").GetComponent<GameRoot>().Spawn();
+        }
     }
     void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Enemy"))
+        if (!other.CompareTag("Enemy") || !other.CompareTag("Boss"))
+        {
             Destroy(obj);
+            Enemy_HP -= 1;
+        }
     }
 }
---
+++ TurtleHunter/Assets/Scripts/UI_Function.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Function : MonoBehaviour {
+
+    bool pause_Clicked;
+	// Use this for initialization
+	void Start () {
+        pause_Clicked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quit();
+	}
+    public void Quit()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+    public void Pause(GameObject _btn)
+    {
+        if(!pause_Clicked)
+        {
+            Time.timeScale = 0.0f;
+            _btn.SetActive(false);
+        }
+        else
+            _btn.SetActive(true);
+    }
+    public void Resume(GameObject _btn)
+    {
+        if (pause_Clicked)
+        {
+            Time.timeScale = 1;
+            _btn.SetActive(true);
+        }
+        else
+            _btn.SetActive(false);
+    }
+}
