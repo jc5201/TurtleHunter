7b31506eaff0d07340f8261fc382a76fbc8148b9
jc5201 <jch5201@naver.com>
2016-08-25 09:40:30 +0900

    InputScript로 통합, 진동

--- TurtleHunter/Assets/Scripts/InputScript.cs
+++ TurtleHunter/Assets/Scripts/InputScript.cs
@@ -4,15 +4,32 @@ using System.Collections;
 public class InputScript : MonoBehaviour {
 
 
-    GameObject Player;
+    private GameObject Player;
+    public GameObject o_bullet;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Player = GameObject.Find("Player");
+        Player.transform.rotation = new Quaternion(0, 0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //플레이어 회전
         Player.transform.Rotate(-1 * Input.gyro.rotationRateUnbiased.x, -1 * Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRateUnbiased.z);
-	}
+
+
+        if (Input.touchCount == 1)//터치
+        //if(Input.GetMouseButton(0))//임시방편 마우스 클릭
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+                Instantiate(o_bullet);
+            Debug.Log("Touchded");
+        }
+    }
+
+    void Touched()
+    {
+        Handheld.Vibrate();
+    }
 }
